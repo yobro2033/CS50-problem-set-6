5d81c92a1df433be7ef390267540e8d3b946d0d4
Quoc Viet Phan <80685605+yobro2033@users.noreply.github.com>
2022-02-05 09:21:50 +0700

    Update credit.py

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,16 +3,14 @@ if len(cardNumber) < 15 or len(cardNumber) > 16:
     print("INVALID")
 else:
     if len(cardNumber) == 15:
-        startPoint = int(cardNumber[0])
-        if startPoint == 3:
+        if cardNumber[0] == 3:
             print("AMEX")
         else:
             print("INVALID")
     elif len(cardNumber) == 16:
-        startPoint = int(cardNumber[0])
-        if startPoint == 4:
+        if cardNumber[0] == 4:
             print("VISA")
-        elif startPoint == 5:
+        elif cardNumber[0] == 5:
             print("MASTERCARD")
         else:
             print("INVALID")
